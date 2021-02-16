0d77a2c89a6d3f688233330132f04524e68db473
Josh Young <j22cal@gmail.com>
2021-02-16 15:48:52 -0600

    Return a list of students

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 
@@ -17,8 +20,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("hello", "test");
+	public List<Student> hello() {
+		return List.of(
+	     new Student(
+	    		1L,
+				"Test",
+				"test@gmail.com",
+				LocalDate.of(1980, Month.JANUARY, 10),
+				21
+		)
+		);
 	}
 
 }
